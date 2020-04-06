898dac474afa8f84bc85bc6cbf6b42354ff76ada
mutua-mutinda <mutuamutinda@outlook.com>
2020-04-06 20:24:22 +0300

    Updates

---
+++ precache-manifest.f3a3a85210b9ec74c97ad5c2e7e5071e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84509f70469093a91ee71357ab6da8f6",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "bafaa09b2e3263c4fd32",
+    "url": "/videos/static/css/main.a882992b.chunk.css"
+  },
+  {
+    "revision": "c4c7e78bef9315119f75",
+    "url": "/videos/static/js/2.3553c668.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/videos/static/js/2.3553c668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bafaa09b2e3263c4fd32",
+    "url": "/videos/static/js/main.3d2cff00.chunk.js"
+  },
+  {
+    "revision": "e9ef7060a80fc9cfd8d4",
+    "url": "/videos/static/js/runtime-main.bef75cc8.js"
+  }
+]);
